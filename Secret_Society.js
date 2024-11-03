b1e7e035a5b9766cf5299ee27a7ab0a70227b570
Manuel-LD <manuel.lade@hotmail.com>
2024-11-03 12:11:00 -0600

    first commit

---
+++ Secret_Society.js
@@ -0,0 +1,9 @@
+//Secret Society
+/*Find the name of a secret society based on the first letter of each member's name.
+ 
+
+Examples:
+- `secretName(["Esperanza", "Franco", "Nia"])` should return `'EFN'`.
+- `secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel'])` should return `'CJMPRR'`.
+- `secretName(['Harry', 'Ron', 'Hermione'])` should return `'HHR'`.*/
+
